3e9550894e58665b4d8540bf35fa91d270f1e23b
Ankur Puri <apuri@iit.edu>
2018-05-20 01:42:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -82,7 +82,9 @@ function createRock(x) {
      * If a rock collides with the DODGER,
      * we should call endGame()
      */
-    
+    if() {
+      
+    }
     /**
      * Otherwise, if the rock hasn't reached the bottom of
      * the GAME, we want to move it again.
