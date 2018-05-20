2cd9c77d51dec0260fd91ca27417959c36c5a0be
Ankur Puri <apuri@iit.edu>
2018-05-20 01:54:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -96,6 +96,7 @@ function createRock(x) {
      * But if the rock *has* reached the bottom of the GAME,
      * we should remove the rock from the DOM
      */
+     
   }
 
   // We should kick of the animation of the rock around here
