a32f6a84f212448769d27ae8ec808e8b62a27459
Ankur Puri <apuri@iit.edu>
2018-05-20 01:53:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -89,7 +89,9 @@ function createRock(x) {
      * Otherwise, if the rock hasn't reached the bottom of
      * the GAME, we want to move it again.
      */
-
+    else {
+      window.requestAnimationFrame(moveRock)
+    }
     /**
      * But if the rock *has* reached the bottom of the GAME,
      * we should remove the rock from the DOM
