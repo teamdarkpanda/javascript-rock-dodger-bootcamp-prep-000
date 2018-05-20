1a4aa1cfe71bd363f0cefafe5fd631d7d785912d
Ankur Puri <apuri@iit.edu>
2018-05-20 01:41:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ function createRock(x) {
   function moveRock() {
     // implement me!
     // (use the comments below to guide you!)
-    rock.style.top = ${top}
+    rock.style.top = `${top += 2}px`
     /**
      * If a rock collides with the DODGER,
      * we should call endGame()
