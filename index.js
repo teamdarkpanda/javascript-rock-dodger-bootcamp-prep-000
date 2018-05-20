b4c5c77a6df71930f20e4e29121446bd275616e2
Ankur Puri <apuri@iit.edu>
2018-05-20 01:18:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -138,14 +138,14 @@ function moveDodgerLeft() {
   function move(el) {
     var leftNumbers = DODGER.style.left.replace('px', '')
     var left = parseInt(leftNumbers, 10)
-    
-    
+    if (left > 0) {
+    DODGER.style.left = `${left - 4}px`
+    }
+    window.requestAnimationFrame(move)
   } 
 
 
-  if (left > 0) {
-    dodger.style.left = `${left - 1}px`
-  }
+  
 }
 
 function moveDodgerRight() {
