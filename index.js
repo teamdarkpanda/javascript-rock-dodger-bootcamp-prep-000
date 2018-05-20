ac6eae5eedbb6b1fd9bd4aa9d37b5af6d347a174
Ankur Puri <apuri@iit.edu>
2018-05-20 02:02:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -98,13 +98,13 @@ function createRock(x) {
      * we should remove the rock from the DOM
      */
     else {
-      ROCKS.push()
       rock.remove()
     } 
   }
 
   // We should kick of the animation of the rock around here
-
+  window.requestAnimationFrame(moveRock)
+  
   // Add the rock to ROCKS so that we can remove all rocks
   // when there's a collision
   ROCKS.push(rock)
